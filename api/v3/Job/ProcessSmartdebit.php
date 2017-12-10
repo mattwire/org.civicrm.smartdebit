0469c09c7533509143503d3060dadc5b5eeb62a4
Matthew Wire <devel@mrwire.co.uk>
2017-12-10 22:57:54 +0900

    Rename sync API to Job.process_smartdebit for consistency

---
+++ api/v3/Job/ProcessSmartdebit.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Smart Debit to CiviCRM Sync
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_job_process_smartdebit($params) {
+  $runner = CRM_Smartdebit_Sync::getRunner(FALSE);
+  if ($runner) {
+    $result = $runner->runAll();
+  }
+  else {
+    return civicrm_api3_create_error('No records could be matched to collection reports.  If you have collection reports in Smartdebit try a manual sync or reconcile.');
+  }
+
+  if ($result && !isset($result['is_error'])) {
+    return civicrm_api3_create_success();
+  }
+  else {
+    $msg = '';
+    if (!empty($result['exception'])) {
+      $msg .= $result['exception']->getMessage() . '; ';
+    }
+    if (!empty($result['last_task_title'])) {
+      $msg .= $result['last_task_title'] .'; ';
+    }
+    return civicrm_api3_create_error($msg);
+  }
+
+}
--- api/v3/Smartdebit.php
+++ api/v3/Smartdebit.php
@@ -24,7 +24,7 @@
  +-------------------------------------------------------------------*/
 
 /**
- * Smart Debit to CiviCRM Sync
+ * Alias for Job.process_smartdebit
  *
  * @param array $params
  * @return array API result descriptor
@@ -33,25 +33,5 @@
  * @throws API_Exception
  */
 function civicrm_api3_smartdebit_sync($params) {
-  $runner = CRM_Smartdebit_Sync::getRunner(FALSE);
-  if ($runner) {
-    $result = $runner->runAll();
-  }
-  else {
-    return civicrm_api3_create_error('No records could be matched to collection reports.  If you have collection reports in Smartdebit try a manual sync or reconcile.');
-  }
-
-  if ($result && !isset($result['is_error'])) {
-    return civicrm_api3_create_success();
-  }
-  else {
-    $msg = '';
-    if (!empty($result['exception'])) {
-      $msg .= $result['exception']->getMessage() . '; ';
-    }
-    if (!empty($result['last_task_title'])) {
-      $msg .= $result['last_task_title'] .'; ';
-    }
-    return civicrm_api3_create_error($msg);
-  }
+  return civicrm_api3_job_process_smartdebit($params);
 }
