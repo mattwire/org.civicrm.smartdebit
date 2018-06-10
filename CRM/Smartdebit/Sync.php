090633f35f26fc9d919273c50f49ad4bd0e99e96
Matthew Wire <devel@mrwire.co.uk>
2018-06-10 13:14:06 +0100

    Fix sync message

--- CRM/Smartdebit/Sync.php
+++ CRM/Smartdebit/Sync.php
@@ -82,7 +82,7 @@ class CRM_Smartdebit_Sync
       $task    = new CRM_Queue_Task(
         array('CRM_Smartdebit_Sync', 'syncSmartdebitCollectionReports'),
         array($start, self::BATCH_COUNT),
-        "Processing Smartdebit collections: {$counter} to " . $start + self::BATCH_COUNT . " of {$count}"
+        "Processing Smartdebit collections: {$counter} to " . ($start + self::BATCH_COUNT) . " of {$count}"
       );
 
       // Add the Task to the Queue
