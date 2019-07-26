f13ec0517e8f6053e7516541cd9a0ea3a8579abd
Matthew Wire (MJW Consulting) <mjw@mjwconsult.co.uk>
2019-07-26 16:04:13 +0100

    CRLF to LF

--- CRM/Core/Payment/Smartdebit.php
+++ CRM/Core/Payment/Smartdebit.php
@@ -1,27 +1,7 @@
 <?php
-/*--------------------------------------------------------------------+
- | CiviCRM version 4.7                                                |
-+--------------------------------------------------------------------+
- | Copyright CiviCRM LLC (c) 2004-2017                                |
-+--------------------------------------------------------------------+
- | This file is a part of CiviCRM.                                    |
- |                                                                    |
- | CiviCRM is free software; you can copy, modify, and distribute it  |
- | under the terms of the GNU Affero General Public License           |
- | Version 3, 19 November 2007 and the CiviCRM Licensing Exception.   |
- |                                                                    |
- | CiviCRM is distributed in the hope that it will be useful, but     |
- | WITHOUT ANY WARRANTY; without even the implied warranty of         |
- | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.               |
- | See the GNU Affero General Public License for more details.        |
- |                                                                    |
- | You should have received a copy of the GNU Affero General Public   |
- | License and the CiviCRM Licensing Exception along                  |
- | with this program; if not, contact CiviCRM LLC                     |
- | at info[AT]civicrm[DOT]org. If you have questions about the        |
- | GNU Affero General Public License or the licensing of CiviCRM,     |
- | see the CiviCRM license FAQ at http://civicrm.org/licensing        |
- +-------------------------------------------------------------------*/
+/*
+ * https://civicrm.org/licensing
+ */
 
 /**
  * Class CRM_Core_Payment_Smartdebit
@@ -305,9 +285,9 @@ class CRM_Core_Payment_Smartdebit extends CRM_Core_Payment {
         'title' => E::ts('Account Holder'),
         'cc_field' => TRUE,
         'attributes' => [
-          'size' => 20
-        , 'maxlength' => 18
-        , 'autocomplete' => 'on'
+          'size' => 20,
+          'maxlength' => 18,
+          'autocomplete' => 'on'
         ],
         'is_required' => TRUE,
         'description' => E::ts('Should be no more than 18 characters. Should not include punctuation (e.g. O\'Callaghan should be OCallaghan). First initial and surname is valid. (e.g. D Watson).'),
@@ -319,9 +299,9 @@ class CRM_Core_Payment_Smartdebit extends CRM_Core_Payment {
         'title' => E::ts('Bank Account Number'),
         'cc_field' => TRUE,
         'attributes' => [
-          'size' => 20
-        , 'maxlength' => 8
-        , 'autocomplete' => 'off'
+          'size' => 20,
+          'maxlength' => 8,
+          'autocomplete' => 'off'
         ],
         'is_required' => TRUE,
         'description' => E::ts('8 digits (e.g. 12345678).'),
@@ -333,9 +313,9 @@ class CRM_Core_Payment_Smartdebit extends CRM_Core_Payment {
         'title' => E::ts('Sort Code'),
         'cc_field' => TRUE,
         'attributes' => [
-          'size' => 20
-        , 'maxlength' => 6
-        , 'autocomplete' => 'off'
+          'size' => 20,
+          'maxlength' => 6,
+          'autocomplete' => 'off'
         ],
         'is_required' => TRUE,
         'description' => E::ts('6 digits (e.g. 01 23 45).'),
@@ -363,9 +343,9 @@ class CRM_Core_Payment_Smartdebit extends CRM_Core_Payment {
         'title' => 'DDI Reference',
         'cc_field' => TRUE,
         'attributes' => [
-          'size' => 20
-        , 'maxlength' => 64
-        , 'autocomplete' => 'off'
+          'size' => 20,
+          'maxlength' => 64,
+          'autocomplete' => 'off'
         ],
         'is_required' => TRUE,
       ]
